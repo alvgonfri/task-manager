4f6de7291ec914d55bd43055493220bda5abc967
Alvaro Gonzalez <alvgonfri@alum.us.es>
2023-08-28 17:03:07 +0200

    Home page responsiveness
    The home page has been adapted to small devices.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,11 +7,11 @@ function HomePage() {
   return (
     <>
       <div className="flex justify-center mt-5">
-        <p className="text-4xl font-bold">
+        <p className="text-4xl font-bold text-center">
           Task Manager lets you organize your tasks in a simple way.
         </p>
       </div>
-      <div className="grid grid-cols-2">
+      <div className="md:grid grid-cols-2">
         <div className="p-5">
           <img
             src="/src/assets/home_image.jpg"
@@ -19,20 +19,20 @@ function HomePage() {
             className="rounded-lg"
           />
         </div>
-        <div className="flex flex-col justify-center items-center m-5">
+        <div className="flex flex-col justify-center items-center m-5 pb-5 md:pb-0">
           {isAuthenticated ? (
             <>
               <p className="text-3xl font-semibold mb-5">Manage your tasks!</p>
               <div className="flex gap-x-4 mt-5">
                 <Link
                   to="/tasks"
-                  className="bg-slate-700 hover:bg-slate-800 text-slate-50 font-medium rounded-lg text-xl px-5 py-2.5"
+                  className="bg-slate-700 hover:bg-slate-800 text-slate-50 font-medium rounded-lg md:text-xl px-5 py-2.5"
                 >
                   View tasks
                 </Link>
                 <Link
                   to="/tasks/create"
-                  className="bg-slate-700 hover:bg-slate-800 text-slate-50 font-medium rounded-lg text-xl px-5 py-2.5"
+                  className="bg-slate-700 hover:bg-slate-800 text-slate-50 font-medium rounded-lg md:text-xl px-5 py-2.5"
                 >
                   New task
                 </Link>
@@ -44,13 +44,13 @@ function HomePage() {
               <div className="flex gap-x-4 mt-5">
                 <Link
                   to="/login"
-                  className="bg-slate-700 hover:bg-slate-800 text-slate-50 font-medium rounded-lg text-xl px-5 py-2.5"
+                  className="bg-slate-700 hover:bg-slate-800 text-slate-50 font-medium rounded-lg md:text-xl px-5 py-2.5"
                 >
                   Login
                 </Link>
                 <Link
                   to="/register"
-                  className="bg-slate-400 hover:bg-slate-500 text-slate-50 font-medium rounded-lg text-xl px-5 py-2.5"
+                  className="bg-slate-400 hover:bg-slate-500 text-slate-50 font-medium rounded-lg md:text-xl px-5 py-2.5"
                 >
                   Register
                 </Link>
