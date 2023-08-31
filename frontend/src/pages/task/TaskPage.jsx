366f7b65d6bad4db943025674ca7292c88f1617d
Alvaro Gonzalez <alvgonfri@alum.us.es>
2023-08-31 15:43:31 +0200

    Change status from task page
    It is now possible to change the task status from the task details page.

--- frontend/src/pages/task/TaskPage.jsx
+++ frontend/src/pages/task/TaskPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useTask } from "../../context/TaskContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
@@ -8,7 +8,8 @@ import BackButton from "../../components/BackButton";
 
 function TaskPage() {
   const [task, setTask] = useState({});
-  const { getTask, deleteTask } = useTask();
+  const { getTask, deleteTask, changeTaskStatus } = useTask();
+  const navigate = useNavigate();
   const params = useParams();
 
   useEffect(() => {
@@ -30,6 +31,7 @@ function TaskPage() {
           <p className="text-xl font-bold text-center break-words">
             {task.title}
           </p>
+
           <div className="flex gap-x-2 justify-center my-2">
             <Link
               to={`/tasks/update/${task._id}`}
@@ -44,12 +46,15 @@ function TaskPage() {
               <FontAwesomeIcon icon={faTrash} className="w-4" />
             </button>
           </div>
+
           {task.description ? (
             <p className="break-words">{task.description}</p>
           ) : (
             <p className="opacity-50">No desc.</p>
           )}
+
           <hr className="border border-slate-300 my-2"></hr>
+
           {task.deadline ? (
             <p>
               <span className="font-semibold">Deadline:</span>{" "}
@@ -58,6 +63,34 @@ function TaskPage() {
           ) : (
             <p className="font-semibold">No deadline</p>
           )}
+
+          <div className="flex justify-center">
+            {task.status === "pending" ? (
+              <button
+                onClick={() =>
+                  changeTaskStatus(task._id, "in-progress") &&
+                  navigate("/tasks")
+                }
+                className="bg-slate-700 text-slate-400 px-2 py-1 mt-1 rounded-md border border-slate-400 hover:bg-slate-400 hover:text-slate-700 hover:border-slate-700 transition duration-500"
+              >
+                <p className="font-semibold">Start</p>
+              </button>
+            ) : (
+              <></>
+            )}
+            {task.status === "in-progress" ? (
+              <button
+                onClick={() =>
+                  changeTaskStatus(task._id, "completed") && navigate("/tasks")
+                }
+                className="bg-slate-700 text-slate-400 px-2 py-1 mt-1 rounded-md border border-slate-400 hover:bg-slate-400 hover:text-slate-700 hover:border-slate-700 transition duration-500"
+              >
+                <p className="font-semibold">Set as completed</p>
+              </button>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
       </div>
     </>
